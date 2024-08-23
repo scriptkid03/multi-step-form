4884a2f9a846fb70c999a27da7c9ac7f66318df9
Edmund Phillips <edmund.phillips@acity.edu.gh>
2024-08-23 00:00:56 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div>
+        Multi-Step Form
+      </div>
+    </main>
+  );
+}
