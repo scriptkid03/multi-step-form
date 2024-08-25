0892f63ed2b78e4d86b767258a2fcf184818354f
Edmund Phillips <edmund.phillips@acity.edu.gh>
2024-08-24 21:00:10 -0700

    left-side done

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div>
-        Multi-Step Form
+    <main className="flex min-h-screen flex-col items-center justify-center bg-lightBlue">
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex w-[65%] h-[75%] bg-white rounded-xl p-4">
+          <div className="flex flex-col w-[30%] h-full bg-sidebar bg-no-repeat bg-center bg-cover rounded-lg">
+            <div className="flex flex-col gap-5 p-12">
+              <div className="flex items-center gap-4">
+                <div className="flex justify-center items-center w-10 h-10 border-2 border-lightBlue rounded-full">
+                  <span className="text-alabaster">1</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-nowrap text-lightGray">STEP 1</span>
+                  <span className="text-nowrap text-alabaster medium">YOUR INFO</span>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <div className="flex justify-center items-center w-10 h-10 border-2 border-lightBlue rounded-full">
+                  <span className="text-alabaster">2</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-nowrap text-lightGray">STEP 2</span>
+                  <span className="text-nowrap text-alabaster medium">SELECT PLAN</span>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <div className="flex justify-center items-center w-10 h-10 border-2 border-lightBlue rounded-full">
+                  <span className="text-alabaster">3</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-nowrap text-lightGray">STEP 3</span>
+                  <span className="text-nowrap text-alabaster medium">ADD-ONS</span>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <div className="flex justify-center items-center w-10 h-10 border-2 border-lightBlue rounded-full">
+                  <span className="text-alabaster">4</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-nowrap text-lightGray">STEP 4</span>
+                  <span className="text-nowrap text-alabaster medium">SUMMARY</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,21 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "sidebar": "url('/svgs/bg-sidebar-desktop.svg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        marineBlue: "hsl(213, 96%, 18%)",
+        purplishBlue: "hsl(243, 100%, 62%)",
+        pastelBlue: "hsl(228, 100%, 84%)",
+        lightBlue: "hsl(206, 94%, 87%)",
+        strawberryRed: "hsl(354, 84%, 57%)",
+        coolGray: "hsl(231, 11%, 63%)",
+        lightGray: "hsl(229, 24%, 87%)",
+        magnolia: "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
       },
     },
   },
